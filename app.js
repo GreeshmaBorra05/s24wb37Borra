d4cf889f39ae8d364a46ac7495350a0a4676dc3a
GreeshmaBorraa <156365934+GreeshmaBorraa@users.noreply.github.com>
2024-03-27 11:51:16 -0500

    class created

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var groceriesRouter = require('./routes/groceries'); //groceries endpoint added
+var gridRouter = require('./routes/grid'); //grid endpoint added
+
 
 
 var app = express();
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/groceries', groceriesRouter); 
+app.use('/grid', gridRouter); //grid endpoint added
 
 
 // catch 404 and forward to error handler
