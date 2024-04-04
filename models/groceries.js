f96a09abd76d0e66edeb55032fa488bacd322bdd
GreeshmaBorraa <156365934+GreeshmaBorraa@users.noreply.github.com>
2024-04-03 20:16:23 -0500

    modified

--- models/groceries.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-const groceriesSchema = mongoose.Schema({
-item_type: String,
-item_name: String,
-item_price: Number
-})
-
-module.exports = mongoose.model("groceries",
-groceriesSchema)
--- routes/groceries.js
+++ routes/groceries.js
@@ -1,6 +1,9 @@
 var express = require('express');
-const groceries_controllers= require('../controllers/groceries');
 var router = express.Router();
-/* GET Groceries */
-router.get('/', groceries_controllers.groceries_view_all_Page );
+ 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('groceries', { title: 'Search Results Groceries' });
+});
+ 
 module.exports = router;
